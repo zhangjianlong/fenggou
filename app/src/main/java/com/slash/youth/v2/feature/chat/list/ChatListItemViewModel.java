20eddba7e61a9b8126846e2418fb7a1d49aae361
zhangjianlong <zhangjianlong805@aliyun.com>
2017-03-31 18:09:50 +0800

    聊天布局

--- app/src/main/java/com/slash/youth/v2/feature/chat/list/ChatListItemViewModel.java
+++ app/src/main/java/com/slash/youth/v2/feature/chat/list/ChatListItemViewModel.java
@@ -1,5 +1,7 @@
 package com.slash.youth.v2.feature.chat.list;
 
+import android.databinding.ObservableField;
+
 import com.core.op.lib.base.BViewModel;
 
 /**
@@ -7,4 +9,10 @@ import com.core.op.lib.base.BViewModel;
  */
 
 public class ChatListItemViewModel extends BViewModel {
+
+    //发送和接收消息的时间间隔  11月7日 14:17
+    public final ObservableField<String> datetimeText = new ObservableField<>();
+    //好友发的文本消息
+
+
 }
