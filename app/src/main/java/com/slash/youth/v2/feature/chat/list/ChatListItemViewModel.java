f6a5f2bc4bf701fadbab6b783b2e7787e40622a0
zhangjianlong <zhangjianlong805@aliyun.com>
2017-04-01 09:30:51 +0800

    修改聊天布局

--- app/src/main/java/com/slash/youth/v2/feature/chat/list/ChatListItemViewModel.java
+++ app/src/main/java/com/slash/youth/v2/feature/chat/list/ChatListItemViewModel.java
@@ -3,6 +3,7 @@ package com.slash.youth.v2.feature.chat.list;
 import android.databinding.ObservableField;
 
 import com.core.op.lib.base.BViewModel;
+import com.core.op.lib.command.ReplyCommand;
 
 /**
  * Created by acer on 2017/3/31.
@@ -11,8 +12,16 @@ import com.core.op.lib.base.BViewModel;
 public class ChatListItemViewModel extends BViewModel {
 
     //发送和接收消息的时间间隔  11月7日 14:17
-    public final ObservableField<String> datetimeText = new ObservableField<>();
-    //好友发的文本消息
+    public final ObservableField<String> infoText = new ObservableField<>();
+
+    //交换手机号码
+    public final ObservableField<String> otherContactInfo = new ObservableField<>();
+    public final ReplyCommand callToOther = new ReplyCommand(() -> {
+
+    });
+    //接受到的图片
+    public final ObservableField<String> imgUrl = new ObservableField<>();
+    //接受
 
 
 }
