f24a7f5750b749645c87ced4caf31c22b46bc0fb
zhangjianlong <zhangjianlong805@aliyun.com>
2017-04-08 12:25:21 +0800

    zjl

--- app/src/main/java/com/slash/youth/v2/feature/profile/ProfileViewModel.java
+++ app/src/main/java/com/slash/youth/v2/feature/profile/ProfileViewModel.java
@@ -2,6 +2,8 @@ package com.slash.youth.v2.feature.profile;
 
 
 import android.databinding.ObservableField;
+import android.text.Editable;
+import android.text.TextWatcher;
 
 import com.core.op.lib.base.BAViewModel;
 import com.core.op.lib.di.PerActivity;
@@ -10,12 +12,35 @@ import com.slash.youth.databinding.ActProfileBinding;
 import com.slash.youth.utils.CommonUtils;
 import com.trello.rxlifecycle.components.support.RxAppCompatActivity;
 
+import java.util.Objects;
+
 import javax.inject.Inject;
 
 @PerActivity
 public class ProfileViewModel extends BAViewModel<ActProfileBinding> {
     public final ObservableField<String> title = new ObservableField<>(CommonUtils.getContext().getString(R.string.app_profile_title));
+    public final ObservableField<String> templateInput = new ObservableField<>();
+    public final ObservableField<String> templateSize = new ObservableField<>(CommonUtils.getContext().getString(R.string.app_profile_template_limit));
 
+
+    public TextWatcher templateWatch = new TextWatcher() {
+        private CharSequence temp;
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            temp = s;
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count,
+                                      int after) {
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            templateSize.set(temp.length() + "/300");
+        }
+    };
 
     @Inject
     public ProfileViewModel(RxAppCompatActivity activity) {
