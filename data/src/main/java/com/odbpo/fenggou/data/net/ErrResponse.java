100ebea66926a32fda8505b57cb09740ceef0ae9
zhangjianlong <zhangjianlong805@aliyun.com>
2017-07-30 22:33:10 +0800

    zjl

---
+++ data/src/main/java/com/odbpo/fenggou/data/net/ErrResponse.java
@@ -0,0 +1,16 @@
+package com.odbpo.fenggou.data.net;
+
+/**
+ * Created by hedong on 2016/4/19.
+ */
+public class ErrResponse {
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String msg;
+}
